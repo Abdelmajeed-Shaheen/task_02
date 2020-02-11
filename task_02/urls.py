0db597a161e678fb29d6ce50e2a8687ef3538ebd
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-02-11 19:31:44 +0200

    test Passed2

--- task_02/urls.py
+++ task_02/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from restaurants.views import hello_world
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',hello_world,name='hello-world'),
